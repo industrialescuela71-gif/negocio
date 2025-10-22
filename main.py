e4817ba8f498cc336fefe374f1f2adc249366f9a
alejorama <fucaraccioalejo@gmail.com>
2025-10-22 19:36:55 -0300

    version de alejo

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
+    
+    def consultar_stock(self):
+        consulta = int(input("consultar stock: "))
+        if self.stock == consulta: 
+            return True
+        else: 
+            return False
 
+remera = Producto("Remera roja", 10000, 50)
 
+remera.consultar_stock()
         
